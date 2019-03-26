c27c3cdf77e146a0a2961b211053ecd5e1d0b043
Dmitriy Dokshin <dmitriy.dokshin@gmail.com>
2019-03-26 18:06:34 +0300

    Add optimal solution for find_equilibrium

--- find_equilibrium/main.cpp
+++ find_equilibrium/main.cpp
@@ -7,25 +7,19 @@
 using namespace std;
 
 template <class T>
-size_t FindEquilibrium(const vector<T>& v) {
+size_t FindEquilibrium(vector<T>& v) {
     static_assert(is_arithmetic<T>::value, "Type has to be arithmetic");
 
     if (v.size() == 1) {
         return 0;
     }
 
-    vector<T> left(v.begin(), v.end());
     for (size_t i = 1; i < v.size(); i++) {
-        left[i] += left[i - 1];
-    }
-
-    vector<T> right(v.begin(), v.end());
-    for (long i = v.size() - 2; i >= 0; i--) {
-        right[i] += right[i + 1];
+        v[i] += v[i - 1];
     }
 
     for (size_t i = 1; i < v.size() - 1; i++) {
-        if (left[i - 1] == right[i + 1]) {
+        if (v[i - 1] == v.back() - v[i]) {
             return i;
         }
     }
@@ -37,7 +31,7 @@ int main() {
     vector<pair<vector<long>, size_t>> tests {
         {{1, 3, 5, 2, 2}, 2}
     };
-    for (const auto& x : tests) {
+    for (auto& x : tests) {
         const bool ok = FindEquilibrium(x.first) == x.second;
         cerr << (ok ? "OK" : "ERROR") << endl;
     }
