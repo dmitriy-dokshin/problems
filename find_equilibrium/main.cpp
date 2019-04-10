7c0c7362b4d6dcde0263ca900f6416efe4735f8f
Dmitriy Dokshin <dmitriy.dokshin@gmail.com>
2019-04-10 20:04:13 +0300

    Add const solution for find_equilibrium

--- find_equilibrium/main.cpp
+++ find_equilibrium/main.cpp
@@ -27,11 +27,32 @@ size_t FindEquilibrium(vector<T>& v) {
     throw runtime_error("Unable to find equilibrium");
 }
 
+template <class T>
+size_t FindEquilibrium(const vector<T>& v) {
+    static_assert(is_arithmetic<T>::value, "Type has to be arithmetic");
+
+    T rightSum = {};
+    T leftSum = {};
+    for (const auto& x : v) {
+        rightSum += x;
+    }
+
+    for (size_t i = 0; i < v.size(); i++) {
+        rightSum -= v[i];
+        if (leftSum == rightSum) {
+            return i;
+        }
+        leftSum += v[i];
+    }
+
+    throw runtime_error("Unable to find equilibrium");
+}
+
 int main() {
     vector<pair<vector<long>, size_t>> tests {
         {{1, 3, 5, 2, 2}, 2}
     };
-    for (auto& x : tests) {
+    for (const auto& x : tests) {
         const bool ok = FindEquilibrium(x.first) == x.second;
         cerr << (ok ? "OK" : "ERROR") << endl;
     }
