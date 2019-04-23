2aa60adc3b34efcfbeceba3d3733f1df6a52bb01
Dmitriy Dokshin <dmitriy.dokshin@gmail.com>
2019-04-23 13:33:05 +0300

    Add count_min_swaps through merge sort

---
+++ count_min_swaps/main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <tuple>
+#include <vector>
+
+using namespace std;
+
+template <class T>
+ostream& operator<<(ostream& out, const vector<T>& v) {
+    out << "{";
+    if (!v.empty()) {
+        out << v[0];
+        for (size_t i = 1; i < v.size(); i++) {
+            out << ", " << v[i];
+        }
+    }
+    out << "}";
+    return out;
+}
+
+template <class T>
+void Merge(vector<T>& v, size_t begin, size_t middle, size_t end, size_t& swaps) {
+    size_t i = begin;
+    size_t j = middle;
+    while (i < middle && j < end) {
+        if (v[j] < v[i]) {
+            auto val = std::move(v[j]);
+            for (size_t k = i; k < j; k++) {
+                v[k + 1] = std::move(v[k]);
+            }
+            v[i] = std::move(val);
+            j++;
+            middle++;
+            swaps++;
+        }
+        i++;
+    }
+}
+
+template <class T>
+void Sort(vector<T>& v, size_t begin, size_t end, size_t& swaps) {
+    if (end - begin < 2) {
+        return;
+    }
+
+    size_t middle = (begin + end) / 2;
+    Sort(v, begin, middle, swaps);
+    Sort(v, middle, end, swaps);
+    Merge(v, begin, middle, end, swaps);
+}
+
+int main() {
+    vector<int> v {3, 1, 3, 3, 2, 1, 5};
+    size_t swaps = 0;
+    Sort(v, 0, v.size(), swaps); 
+    cerr << v << endl;
+    cerr << swaps << endl;
+}
