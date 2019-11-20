10b2e3b51fe61cafdfa07ac118111e156827be56
Dmitriy Dokshin <dmitriy.dokshin@gmail.com>
2019-11-21 01:39:26 +0300

    Add look_and_say

---
+++ facebook/look_and_say/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+template <class T>
+ostream& operator<<(ostream& out, const vector<T>& v) {
+    for (const auto& x : v) {
+        out << x;
+    }
+    return out;
+}
+
+void lool_and_say(const size_t n) {
+    vector<int> v{1};
+    for (size_t i = 0; i < n; i++) {
+        cerr << v << endl;
+
+        vector<int> next;
+        int val = v[0];
+        int l = 1;
+        for (size_t j = 1; j < v.size(); j++) {
+            if (v[j] == val) {
+                l++;
+            } else {
+                next.emplace_back(l);
+                next.emplace_back(val);
+                val = v[j];
+                l = 1;
+            }
+        }
+        next.emplace_back(l);
+        next.emplace_back(val);
+        v = std::move(next);
+    }
+}
+
+int main(const int argc, const char* argv[]) {
+    size_t n = 10;
+    if (argc > 1) {
+        n = stoi(argv[1]);
+    }
+
+    lool_and_say(n);
+}
