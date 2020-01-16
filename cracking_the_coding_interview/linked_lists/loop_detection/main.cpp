aaba3a78635fbd756d6f239d7455d388a5683c44
Dmitriy Dokshin <dmitriy.dokshin@gmail.com>
2020-01-16 08:11:49 +0300

    Fix loop_detection

--- cracking_the_coding_interview/linked_lists/loop_detection/main.cpp
+++ cracking_the_coding_interview/linked_lists/loop_detection/main.cpp
@@ -12,64 +12,63 @@ public:
 };
 
 template <class T>
-ostream& operator<<(ostream& out, const TNode<T>* x) {
-    if (x) {
-        out << x->Value;
-        while (x->Next) {
-            out << " -> " << x->Next->Value;
-            x = x->Next;
-        }
-    }
-    return out;
-}
-
-template <class T>
 vector<TNode<T>> MakeList(const vector<T>& v) {
-    vector<TNode<T>> result;
-    result.reserve(v.size());
-    for (size_t i = 0; i < v.size(); i++) {
-        result.emplace_back(TNode<T>{v[i]});
-        if (result.size() > 1) {
-            result[result.size() - 2].Next = &result[result.size() - 1];
+    vector<TNode<T>> list;
+    list.reserve(v.size());
+    if (!v.empty()) {
+        list.emplace_back(TNode<T>{v[0]});
+        for (size_t i = 1; i < v.size(); i++) {
+            list.emplace_back(TNode<T>{v[i]});
+            list[i - 1].Next = &list[i];
         }
     }
-    return result;
+    return list;
 }
 
 template <class T>
 const TNode<T>* FindLoop(const TNode<T>* head) {
-    const auto* fast = head;
     const auto* slow = head;
-    size_t i = 0;
-    do {
-        fast = fast->Next;
-        if (i % 2 == 1) {
-            slow = slow->Next;
-        }
-        i++;
-    } while (fast && fast != slow);
+    const auto* fast = head;
 
-    if (fast) {
-        fast = head;
-        if (i % 2 == 1) {
-            // WTF???
-            slow = slow->Next;
-        }
-        while (fast != slow) {
-            fast = fast->Next;
-            slow = slow->Next;
+    while (fast && fast->Next) {
+        slow = slow->Next;
+        fast = fast->Next->Next;
+        if (slow == fast) {
+            break;
         }
     }
 
+    if (!fast || !fast->Next) {
+        return nullptr;
+    }
+
+    slow = head;
+    while (slow != fast) {
+        slow = slow->Next;
+        fast = fast->Next;
+    }
+
     return fast;
+
 }
 
 int main() {
-    vector<int> v = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    vector<size_t> v = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     for (size_t i = 0; i < v.size(); i++) {
         auto list = MakeList(v);
+
         list[list.size() - 1].Next = &list[i];
-        const auto* loopHead = FindLoop(&list[0]);
-        cerr << (loopHead && static_cast<size_t>(loopHead->Value) == i ? "OK" : "ERROR") << endl;
+
+        const auto* loop_head = FindLoop(&list[0]);
+
+        cerr << i << "\t";
+        if (!loop_head) {
+            cerr << "loop head is null";
+        } else if (loop_head->Value != i) {
+            cerr << "loop head is wrong: " << loop_head->Value;
+        } else {
+            cerr << "ok";
+        }
+        cerr << endl;
     }
 }
